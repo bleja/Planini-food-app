45efa29def00cb8a81d2b789d9231c09c192182b
bleja <58110656+bleja@users.noreply.github.com>
2021-02-20 21:51:36 +0100

    Updates

--- README.md
+++
@@ -1,8 +0,0 @@
-This is the final project for the Front End Development course @ CodersLab.
-
-It is an application helping to create a weekly meal plan and grocery shopping list. 
-Features planned for the future:
-- possibility to count the macro ingredients of the daily/weekly plan.
-- possibility to export the shopping list into Listonic App (TBD).
-
-
---
+++ precache-manifest.782294a0ef37b75f5c7b4087ccc5f0a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9800f5868f59dda47ac7471ed0b9fece",
+    "url": "/Planini-food-app/index.html"
+  },
+  {
+    "revision": "7c1380d1cf7916873e02",
+    "url": "/Planini-food-app/static/css/main.68a01198.chunk.css"
+  },
+  {
+    "revision": "71d59ac4d926fa4fdf8b",
+    "url": "/Planini-food-app/static/js/2.8f7bd5c7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Planini-food-app/static/js/2.8f7bd5c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c1380d1cf7916873e02",
+    "url": "/Planini-food-app/static/js/main.52d9a955.chunk.js"
+  },
+  {
+    "revision": "58eb24937c5df3139b8f",
+    "url": "/Planini-food-app/static/js/runtime-main.66fd0f15.js"
+  }
+]);
